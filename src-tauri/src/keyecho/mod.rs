b9fcd761abc153e1bd1956c8a9cca1c113aa6b8f
Zack <islizeqiang@gmail.com>
2024-06-13 09:23:14 +0800

    refactor: decouple playback from soundpack

--- src-tauri/src/keyecho/mod.rs
+++ src-tauri/src/keyecho/mod.rs
@@ -2,51 +2,32 @@ use std::{collections::HashSet, sync::Mutex};
 
 use anyhow::{anyhow, Result};
 
-pub(super) mod decoder;
-pub(super) mod listen_key;
+mod echo;
+mod listen_key;
 mod soundpack;
 
-pub use soundpack::{KeySoundpack, SoundOption};
+pub(super) use echo::AudioSource;
+pub(super) use listen_key::{Key, KeyEvent};
+pub(crate) use soundpack::{KeySoundpack, SoundOption};
 
-use crate::global_state::ArcKeySoundpack;
-use listen_key::{listen, Key, ListenKeyEvent};
+pub fn run_keyecho(soundpack: crate::global_state::ArcKeySoundpack) -> Result<()> {
+    let player = echo::SoundPlayer::new(soundpack);
 
-pub fn run_keyecho(soundpack: ArcKeySoundpack) -> Result<()> {
-    let (_stream, stream_handle) = rodio::OutputStream::try_default()?;
     let key_depressed = Mutex::new(HashSet::<Key>::new());
+    let handle_key_event = move |evt: KeyEvent, key_action: &dyn Fn(&mut HashSet<Key>) -> bool| {
+        if key_depressed
+            .lock()
+            .ok()
+            .and_then(|mut depressed| key_action(&mut depressed).then_some(()))
+            .is_some()
+        {
+            player.try_play(evt);
+        }
+    };
 
-    listen(move |evt| match evt {
-        ListenKeyEvent::KeyRelease(k) => {
-            key_depressed
-                .lock()
-                .ok()
-                .map(|mut depressed| depressed.remove(&k));
-        }
-        ListenKeyEvent::KeyPress(k) => {
-            if key_depressed
-                .lock()
-                .ok()
-                .and_then(|mut depressed| depressed.insert(k).then_some(()))
-                .is_some()
-            {
-                soundpack
-                    .lock()
-                    .ok()
-                    .as_mut()
-                    .and_then(|soundpack| {
-                        soundpack
-                            .key_source(k)
-                            .map(|source| (soundpack.volume(), source))
-                    })
-                    .and_then(|(volume, source)| {
-                        rodio::Sink::try_new(&stream_handle).ok().map(|sink| {
-                            sink.append(source);
-                            sink.set_volume(volume);
-                            sink.detach();
-                        })
-                    });
-            }
-        }
+    listen_key::listen(move |evt| match evt {
+        KeyEvent::KeyPress(k) => handle_key_event(evt, &|depressed| depressed.insert(k)),
+        KeyEvent::KeyRelease(k) => handle_key_event(evt, &|depressed| depressed.remove(&k)),
     })
     .map_err(|err| anyhow!(err))
 }
